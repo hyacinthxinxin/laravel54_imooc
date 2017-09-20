cfb619055b64b89a3b94fda33e3f51957726f73b
hyacinthxinxin <fanxin0202@163.com>
2017-09-20 20:54:15 +0800

    change lenght

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,21 +18,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         \Carbon\Carbon::setLocale('zh');
 
-        \View::composer('layout.sidebar', function ($view) {
-            $topics = Topic::all();
-            $view->with('topics', $topics);
-        });
-        DB::listen(function ($query) {
-            $sql = $query->sql;
-            $bindings = $query->bindings;
-            $time = $query->time;
-            // 只打印查询大于10ms的查询
-            if ($time > 10) {
-                Log::debug(var_export(compact('sql', 'bindings', 'time'), true));
-            }
-        });
+//        \View::composer('layout.sidebar', function ($view) {
+//            $topics = Topic::all();
+//            $view->with('topics', $topics);
+//        });
+
+//        DB::listen(function ($query) {
+//            $sql = $query->sql;
+//            $bindings = $query->bindings;
+//            $time = $query->time;
+//            // 只打印查询大于10ms的查询
+//            if ($time > 10) {
+//                Log::debug(var_export(compact('sql', 'bindings', 'time'), true));
+//            }
+//        });
     }
 
     /**
