807d0fbd4ec2500fb21e7dd3b2e341f7c477f6b6
hyacinthxinxin <fanxin0202@163.com>
2017-09-21 14:22:36 +0800

    opt3

---
+++ app/Admin/Controllers/RoomController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fanxin
+ * Date: 2017/9/14
+ * Time: 下午11:50
+ */
+
+namespace App\Admin\Controllers;
+
+
+use App\Topic;
+
+class RoomController extends Controller
+{
+
+}
